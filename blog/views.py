825275a7c7999a71a66a23dec691fb4b7fbda105
ysy2565@naver.com <ysy2565@naver.com>
2021-05-14 23:50:36 +0900

    blog_project

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Blog
+
+def home(request):
+  blogs = Blog.objects #쿼리셋 #메소드
+  return render(request, 'home.html', {'blogs':blogs})
+
+  # 쿼리셋과 메소드의 형식
+  # 모델.쿼리셋(objects).메소드
+def detail(request, blog_id):
+  blog_detail = get_object_or_404(Blog, pk = blog_id)
+  return render(request,'detail.html', {'blog':blog_detail})
